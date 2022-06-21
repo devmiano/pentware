9d0d748332453fbdcbcb73853023118761f611b7
Aliyo254 <alinurali254@gmail.com>
2022-06-21 10:27:23 +0300

    Models 2

--- paint/models.py
+++ paint/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.contrib.auth import AbstractUser
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
+
+class User(AbstractUser):
+    is_customer = models.BooleanField(default=False)
+    is_retailer = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
@@ -20,6 +25,7 @@ class Category(models.Model):
 
 
 class Retailer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     name = models.CharField(max_length=50)
     account = models.ForeignKey("Account", on_delete=models.CASCADE)
     products=models.ForeignKey("Product",on_delete=models.CASCADE)
@@ -32,20 +38,22 @@ class Review(models.Model):
 
 
 class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     name = models.CharField(max_length=50)
-    address = models.IntegerField(max_length=60)
+    address = models.IntergerField(max_length=60)
     city = models.TextField(max_length=20)
     county= models.TextField(max_length=20)
     account = models.ForeignKey("Account", on_delete=models.CASCADE)
 
-class Account(models.Model):
+class Account(User):
     name = models.CharField(max_length=50)
     phone_number= models.IntegerField(max_length=60)
     email= models.EmailField(max_length=20)
     bio= models.TextField(max_length=100)
     
 
-class Admin(models.Model):
+class Admin(User):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     name = models.CharField(max_length=50)
     email= models.EmailField(max_length=60)
     account = models.ForeignKey("Account", on_delete=models.CASCADE)
