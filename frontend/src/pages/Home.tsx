2fbaec8c52ac48bd8f5aaa1d6372a62b0ea14aff
Sammy Muchiri <dev.miano@gmail.com>
2022-06-24 10:28:33 +0300

    heroku settings

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import Title from '../components/Title';
+import Products from './Products';
 function Home() {
 	const heading = Title({ title: 'Welcome to PentWare' });
 
-	return <div className='p-8'>{heading}</div>;
+	return (
+		<div className='p-8'>
+			{heading}
+			<Products />
+		</div>
+	);
 }
 
 export default Home;
