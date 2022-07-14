6fd54e437ee1f4fcc6db6fc83f05e82c4764f06b
Aprilwairimu <wairimuapril@gmail.com>
2022-07-14 12:41:57 +0300

    created model serializers

--- content/cheruiyot/paint/serializers.py
+++ content/cheruiyot/paint/serializers.py
@@ -2,48 +2,49 @@ from rest_framework import serializers
 from .models import User,Product,Category,Retailer,Review,Customer,Account,Admin
 from django.contrib.auth.models import AbstractUser
 
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['name','price', 'product_image','description', 'quantity','Category',' Retailer']
+        fields = ['name','price','product_image','description','quantity','Category',' Retailer']
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['name','category_image','products']
+        fields = ['name','category_image', 'products']
 
 
 class RetailerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Retailer
-        fields = ['name','account','products']
+        fields = ['name', 'account','products']
 
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = ['rate','content', 'products']
+        fields = ['rate','content','products']
 
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ['name','address', 'city','county', 'account']
+        fields = ['name','address','city','county','account']
 
 
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account 
-        fields = ['name', 'phone_number','email', 'bio']
+        fields = ['name','phone_number','email','bio']
 
 
 class AdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = Admin
-        fields =  ['name','email','account']
+        fields =  ['name','email', 'account']
 
 
 class AbstractUserSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ['is_customer','is_retailer','is_admin']
+        fields = ['is_customer', 'is_retailer', 'is_admin']
